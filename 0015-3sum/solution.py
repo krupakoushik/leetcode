31b3e8cfa70bccd62d35ac68183e87bab8c24cd9
Krupa Koushik <krupakoushikkona@gmail.com>
2026-07-04 12:06:57 +0000

    [LeetCode Sync] Runtime - 505 ms (86.16%), Memory - 22.1 MB (81.31%)

--- 0015-3sum/solution.py
+++ 0015-3sum/solution.py
@@ -3,26 +3,36 @@ class Solution:
         # -4, -1, -1, 0, 1, 2
         sol = []
         nums.sort()
+
         for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            left = i + 1
-            right = len(nums) - 1
 
-            while (left < right):
-                sums = nums[i] + nums[left] + nums[right]
+            if nums[i] > 0:
+                break
 
-                if (sums == 0):
-                    sol.append([nums[i], nums[left], nums[right]])
-                    left += 1
+            else:
+                if i > 0 and nums[i] == nums[i - 1]:
+                    continue
 
-                    while nums[left] == nums[left - 1] and left < right:
+                left = i + 1
+                right = len(nums) - 1
+
+                while left < right:
+                    sums = nums[i] + nums[left] + nums[right]
+
+                    if sums == 0:
+                        sol.append([nums[i], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                        
+                    elif sums < 0:
                         left += 1
 
-                elif (sums < 0):
-                    left += 1
+                    else:
+                        right -= 1
 
-                else:
-                    right -= 1
-
-        return sol
+        return sol  
+                
